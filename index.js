5fb9e7d67aab9b3527a696b210b0b6917420be4e
Abdou Sanda Dourhamane <abdous2004@yahoo.fr>
2018-08-26 15:02:13 +0200

    lines of code added for saying we are on the master brach

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ server.use(compression());
 server.use(helmet());
 
 server.get("/", (Request, Response) => {
-    Response.send("you are on the master branch");
+    Response.send("you are on my master branch");
 
 })
 
